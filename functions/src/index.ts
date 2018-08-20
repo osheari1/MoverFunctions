f46f599df97a45a18e258ecd8f7a5de7f92eda3d
osheari1@gmail.com <osheari1@gmail.com>
2018-08-19 19:37:18 -0700

    Add job acceptance function for drivers

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -23,20 +23,115 @@ class Utils {
 
 
 // Listen for a new job request and send out push notifications to drivers
-// TODO: Think about better way of randomly pushing to drivers
-
-// TODO: Need to set up a channel that alerts the notify whether or not the driver accepted the job
-function notifyDriverAndWaitForResponse(deviceToken: string, payload: any) {
-    // TODO: Turn this into an async - await function
-     admin.messaging().sendToDevice(deviceToken, payload)
+function notifyDriverAndWaitForResponse(
+    deviceToken: string,
+    payload: any
+): Promise<void> {
+     return admin.messaging().sendToDevice(deviceToken, payload)
         .then(messagingDeviceResponse => {
             console.log('Messaged pushed.');
             console.log(messagingDeviceResponse);
+            console.log(payload);
         }, err => {
             console.log("Could not push message");
             console.log(err);
         });
+}
 
+function isJobAccepted(change: any): boolean {
+    const data = change.doc.data();
+    return data.accept === true;
+}
+
+
+
+function listenToChanges(requestId: string, driverId: string): Promise<any> {
+    return new Promise((resolve, reject) => {
+        admin.firestore().collection('acceptRejectTimeout')
+            .doc(`${requestId}`)
+            .collection('drivers')
+            .onSnapshot(snap => {
+                snap.docChanges.forEach(change => {
+                    if (change.doc.id == driverId){
+                        console.log(`Request response for ${driverId}: `, change.doc.data());
+                        resolve(change);
+                    }
+                });
+            });
+    });
+}
+
+
+async function notifyDrivers(
+    requestId: string,
+    requestData: any,
+    driverArray: Array<any>): Promise<boolean> {
+    // const driverData = driverArray[0];
+    if (driverArray.length === 0) {
+        return Promise.resolve(false);
+    }
+    const driverData = driverArray.pop();
+    const driverId = driverData[0];
+    const data = driverData[1];
+    const payload = {
+        notification: {
+            title: `Job request ${requestId} available.`,
+            body: `${requestData.clientRef.id} has requested a job.`,
+            sound: "default",
+            click_action: "FCM_PLUGIN_ACTIVITY"
+        },
+        "data": {
+            driverId: driverId,
+            clientId: requestData.clientRef.id,
+            requestId: requestId
+        }
+    };
+
+    console.log(`Sending push notification to ${driverId}`);
+    return Promise.all([
+        notifyDriverAndWaitForResponse(data.deviceToken, payload),
+        listenToChanges(requestId, driverId)
+    ]).then(values => {
+        console.log(`Response from ${driverId} retrieved.`);
+        const changeData = values[1];
+        const jobAccepted = isJobAccepted(changeData);
+        console.log(`Job accepted by ${driverId}: `, jobAccepted);
+        if (jobAccepted) {
+            return jobAccepted;
+        } else {
+            return notifyDrivers(requestId, requestData, driverArray);
+        }
+    }, err => {
+        console.log('Error occurred when notifying drivers: ',
+            JSON.stringify(err));
+        return false;
+    });
+}
+
+function retryNotifyDrivers(
+    notifyData: Array<any>,
+    maxRetries: number,
+): Promise<boolean> {
+    const requestId = notifyData[0];
+    const requestData = notifyData[1];
+    const driverArray = notifyData[2];
+
+    // TODO: Only retry for drivers that timed out
+    return notifyDrivers(requestId, requestData, driverArray)
+        .then(jobAccepted => {
+        if (jobAccepted) {
+            return jobAccepted;
+        } else if (maxRetries > 0) {
+            return retryNotifyDrivers(
+                notifyData,
+                maxRetries - 1);
+        } else {
+            return false;
+        }
+    }, err => {
+        console.log(err);
+        return false
+    })
 }
 
 exports.notifyDriversOfNewJobRequests = functions.firestore
@@ -47,36 +142,25 @@ exports.notifyDriversOfNewJobRequests = functions.firestore
         // Get a list of current drivers
         const driverRef = admin.firestore().collection('driverProfile');
         return driverRef.get().then(driverSnap => {
-            // Get driver data
+            // Get driver data and shuffle
             let driverArray = Utils.shuffleArray(driverSnap.docs.map(doc => [doc.id, doc.data()]));
-            // Shuffle driver data
-            // Utils.shuffleArray(driverSnap.docs.map(doc => [doc.id, doc.data()]))
-            for (let driverData of driverArray) {
-                const driverId = driverData[0];
-                const data = driverData[1];
-                const payload = {
-                    notification: {
-                        title: `Job request ${requestId} available.`,
-                        body: `${requestData.clientRef.id} has requested a job.`,
-                        sound: "default",
-                        click_action: "FCM_PLUGIN_ACTIVITY"
-                    },
-                    "data": {
-                        driverId: driverId,
-                        clientId: requestData.clientRef.id,
-                        requestId: requestId
-                    }
-                };
-                notifyDriverAndWaitForResponse(data.deviceToken, payload);
-                // return admin.messaging().sendToDevice(data.deviceToken, payload)
-                //     .then(messagingDeviceResponse => {
-                //         console.log('Messaged pushed.');
-                //         console.log(messagingDeviceResponse);
-                //     }, err => {
-                //         console.log("Could not push message");
-                //         console.log(err);
-                //     });
-            };
+            // retryNotifyDrivers(
+            //     [requestId, requestData, driverArray],
+            //     3,
+            // ).then(jobAccepted => {
+            //     console.log('Drivers notified');
+            //     console.log('Job accepted: ', jobAccepted);
+            // }, err => {
+            //     console.log(
+            //         'Error in retryNotifyDrivers',
+            //         JSON.stringify(err));
+            // });
+            notifyDrivers(requestId, requestData, driverArray)
+                .then(jobAccepted => {
+                    console.log('Drivers notified');
+                }, err => {
+                    console.log(err);
+                });
         }, err => {
             console.log(err);
         });
